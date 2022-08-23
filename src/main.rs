a48eb5573302c288768d8fe20cd72bdb54cbb68c
tlras <14915607+tlras@users.noreply.github.com>
2022-08-23 13:43:04 -0500

    prevent cmd prompt from opening

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+#![no_main]
+#![windows_subsystem = "windows"]
+
 mod winapi;
 mod routines;
 mod sounds;
 use std::{thread::{self, JoinHandle}};
 
-fn main() {
+#[no_mangle]
+fn main(_argc: i32, _argv: *const *const u8) {
     let mut thread_pool = Vec::<JoinHandle<()>>::with_capacity(routines::THREADS.len());
     for i in routines::THREADS {
         thread_pool.push(thread::spawn(i));
